62fb7ba42794d3b712483420e80a9803d964404f
Edinsson-G <edinssonfernandogutierrez@gmail.com>
2024-07-10 18:09:15 -0500

    boton para cargar y boton para pintar

--- graficaCSV.java
+++ graficaCSV.java
@@ -3,18 +3,21 @@ import javax.swing.filechooser.FileNameExtensionFilter;
 import java.awt.*;
 //clase que implementa la interfaz gŕafica
 public class graficaCSV{
-    //canvas en el que trazará el dibujo
-    //panel en el que mostrara en formato de tabla el resumen del csv
-    private JPanel tabla;
-    //csv actual 
+    public static csv pinturas;
     public static void main(String[] args){
         JFrame ventIinicial=new JFrame("GraficaCSV+");
         ventIinicial.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         ventIinicial.setSize(400,300);
 
-        JLabel mensajeBienvenida= new JLabel("<html>Bienvenido a graficaCSV+, para comenzar carga un archivo CSV.</html>",SwingConstants.CENTER);
-        JButton carguador=new JButton("carguar archivo");
+        //texto de bienvenida
+        JLabel mensajeBienvenida= new JLabel("<html>Bienvenido a graficaCSV+, para pintar carga un archivo CSV.</html>",SwingConstants.CENTER);
 
+        //panel en el que se guardarán los botones de cargar y pintar
+        JPanel botones=new JPanel();
+        //boton para cargar archivo
+        JButton carguador=new JButton("carguar archivo");
+        //boton para generar figura
+        JButton pintar=new JButton("pintar");
         //funiconalidad del boton carguador
         carguador.addActionListener(
             e -> {
@@ -28,28 +31,38 @@ public class graficaCSV{
                 if(navegador.getSelectedFile()!=null)
                 {
                     try{
-                        JFrame lienzo=new JFrame();
-                        csv pinturas=new csv(navegador.getSelectedFile().getAbsolutePath());
-                        lienzo.getContentPane().add(BorderLayout.CENTER,pinturas);
-                        lienzo.setSize(pinturas.ancho(),pinturas.alto());
-                        lienzo.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-                        lienzo.setResizable(false);
-                        lienzo.setVisible(true);
-                        //si la carga es exitosa cerrar la ventana de bienvenida
-                        //ventIinicial.setVisible(false);
-                        //ventIinicial.dispose();
+                        pinturas=new csv(navegador.getSelectedFile().getAbsolutePath());
+                        pintar.setEnabled(true);
+                        mensajeBienvenida.setText("se ha cargado "+navegador.getSelectedFile().getAbsolutePath());
                     }catch(Exception i){
                         JOptionPane.showMessageDialog(null,"Nombre del error: "+i.getClass().getSimpleName(),"No se pudo leer el archivo",JOptionPane.ERROR_MESSAGE);
                     }
                 }
             }
         );
+        pintar.addActionListener(
+            e -> {
+                if(pinturas!=null){
+                    JFrame lienzo=new JFrame();
+                    lienzo.getContentPane().add(BorderLayout.CENTER,pinturas);
+                    lienzo.setSize(pinturas.ancho(),pinturas.alto());
+                    lienzo.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+                    lienzo.setResizable(false);
+                    lienzo.setVisible(true);
+                }else{
+                    JOptionPane.showMessageDialog(null,"Error inesperado, no se encontró ningún archivo CSV cargado","Error inesperado",JOptionPane.ERROR_MESSAGE);
+                }
+            }
+        );
+        pintar.setEnabled(false);
+
+        //agregar los botones al panel
+        botones.add(carguador);
+        botones.add(pintar);
+
         //agregar componentes a la ventana y hacer visible
-        ventIinicial.getContentPane().add(BorderLayout.LINE_END,mensajeBienvenida);
-        ventIinicial.getContentPane().add(BorderLayout.SOUTH,carguador);
-        
+        ventIinicial.getContentPane().add(BorderLayout.NORTH,mensajeBienvenida);
+        ventIinicial.getContentPane().add(BorderLayout.CENTER,botones);
         ventIinicial.setVisible(true);
-            
-        
     }
 }
